2734106d2ffa3538de8f0b052432a755a77b2480
Nayna <shaiknayna294@gmail.com>
2022-01-10 20:05:46 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace LearnTwilio
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+            var accountSid=secrets.AccountSID;
+            var authtoken=secrets.AuthToken;
+            var toNumber=secrets.ToNumber;
+            var fromNumber=secrets.FromNumber;
+
+            TwilioClient.Init(accountSid,authtoken);
+
+            var message = MessageResource.Create(
+                to: new PhoneNumber(toNumber),
+                from:new PhoneNumber(fromNumber),
+                body: "Hey you have got the GREAT CR!!!"
+            );
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
---
+++ secrets.cs
@@ -0,0 +1,13 @@
+namespace LearnTwilio
+{
+    class secrets
+    {
+        public static string ToNumber {get; set;} = "123456";
+        public static string FromNumber {get; set;} = "7891999";
+        public static string AccountSID {get; set;} = "hdaufha53893494";
+
+        public static string AuthToken {get; set;}= "hidari13245678";
+
+       
+    }
+}
